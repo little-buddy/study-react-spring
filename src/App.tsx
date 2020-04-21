3b4be155063668dcb64eefd8b2f761765b9d0530
buddy <litoubuddy@gmail.com>
2020-04-21 18:14:44 +0800

    add: ScrollParallax 模块

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 // import FlipCard from '@/spring-demo/FlipCard';
 // import GestureSlider from '@/spring-demo/GestureSlider';
-import MouseParallax from '@/spring-demo/MouseParallax';
+// import MouseParallax from '@/spring-demo/MouseParallax';
+import ScrollParallax from '@/spring-demo/ScrollParallax';
 
 function App() {
 	// return <FlipCard />;
 	// return <GestureSlider />;
-	return <MouseParallax />;
+	return <ScrollParallax />;
 }
 
 export default App;
